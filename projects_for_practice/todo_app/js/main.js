053d70911718162febb5e9bcf8bef3ab97accee5
Nilender-Andrey <Nilenderav@mail.ru>
2022-08-22 08:02:40 +0400

    feat: todo app / add basic logic

--- projects_for_practice/todo_app/js/main.js
+++ projects_for_practice/todo_app/js/main.js
@@ -13,9 +13,10 @@ function addTask(event) {
 
   const taskHTML = `<li class="todo-list__item task">
                       <div class="task__name">${taskText}</div>
-
-                      <button class="task__btn task__btn_done"></button>
-                      <button class="task__btn task__btn_delete"></button>
+                      <div class="task__btns">
+                        <button class="task__btn task__btn_done" data-action="done"></button>
+                        <button class="task__btn task__btn_delete" data-action="delete"></button>
+                      </div>
                     </li>`;
 
   todoList.insertAdjacentHTML('beforeend', taskHTML);
@@ -23,15 +24,37 @@ function addTask(event) {
   addTaskInput.value = '';
   addTaskInput.focus();
 
-  if (todoList.children.length > 1) {
-    emptyList.classList.add('none');
-  }
+  changeEmptyList();
 }
 
 function todoListClickHandler(event) {
   const target = event.target;
-  if (target.classList.contains('.task__btn_done')) {
+
+  if (target.dataset.action === 'delete') {
+    const parentNode = target.closest('.task');
+    deleteTask(parentNode);
+
+    changeEmptyList();
+  }
+
+  if (target.dataset.action === 'done') {
+    const parentNode = target.closest('.task');
+    taskStatusToggle(parentNode);
   }
 }
 
-function deleteTask(params) {}
+function deleteTask(parentNode) {
+  parentNode.remove();
+}
+
+function changeEmptyList() {
+  if (todoList.children.length > 1) {
+    emptyList.classList.add('none');
+  } else {
+    emptyList.classList.remove('none');
+  }
+}
+
+function taskStatusToggle(parentNode) {
+  parentNode.classList.toggle('done');
+}
